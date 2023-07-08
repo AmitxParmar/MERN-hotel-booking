3831d8fd0eb51673891211288b5234518b28cdc5
AmitxParmar <amitparmar901@gmail.com>
2023-07-08 13:32:23 +0530

    setup: backend

--- api/index.js
+++ api/index.js
@@ -1,1 +1,47 @@
-import express from 'express'
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+dotenv.config();
+
+// Setup Database connection! :: ->
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("Connected to mongoDB.");
+  } catch (error) {
+    throw error;
+  }
+};
+
+mongoose.connection.on("disconnected", () => {
+  console.log("MongoDB disconnected!!");
+});
+
+// Middlewares:: ->
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+
+// Routes:: ->
+
+// Error handling:: ->
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something went wrong!";
+  return res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errorMessage,
+    stack: err.stack,
+  });
+});
+
+// Server port:: ->
+app.listen(8800, () => {
+  connect();
+  console.log("Connected to backend.");
+});
