d2dad74c389b25ed602200b1d56a2cce0e295ca3
AmitxParmar <amitparmar901@gmail.com>
2023-07-09 17:00:39 +0530

    adminVerification->hotels

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -9,16 +9,18 @@ import {
   getHotels,
   updateHotel,
 } from "../controllers/hotel.js";
+import { verifyAdmin } from "../utils/verifyToken.js";
+
 const router = Router();
 
 // Create
-router.post("/", createHotel);
+router.post("/", verifyAdmin, createHotel);
 
 // Update
-router.put("/:id", updateHotel);
+router.put("/:id", verifyAdmin, updateHotel);
 
 // Delete
-router.delete("/:id", deleteHotel);
+router.delete("/:id", verifyAdmin, deleteHotel);
 
 // Get single hotel
 router.get("/find/:id", getHotel);
@@ -29,6 +31,5 @@ router.get("/", getHotels);
 router.get("/countByCity", countByCity);
 router.get("/countByType", countByType);
 router.get("/room/:id", getHotelRooms);
-
 
 export default router;
