af7740b85036ae4fa3913f4b123335e5f223f982
AmitxParmar <amitparmar901@gmail.com>
2023-07-11 00:00:20 +0530

    complete:login

--- client/src/pages/login/Login.tsx
+++ client/src/pages/login/Login.tsx
@@ -9,11 +9,44 @@ const Login = () => {
   });
   const navigate = useNavigate();
 
-  return <div className="login">
-    <div className="lContainer">
-      <input type="password" className="lInput" />
+  const handleChange = (e: React.SyntheticEvent) => {
+    setCredentials((prev) => ({ ...prev, [e.target.id]: e.target.value }));
+  };
+
+  const handleClick = async (e): void => {
+    e.preventDefault();
+    try {
+      const res = await axios.post("/auth/login", credentials);
+      navigate("/");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <div className="login">
+      <div className="lContainer">
+        <input
+          type="text"
+          placeholder="username"
+          id="username"
+          onChange={handleChange}
+          className="lInput"
+        />
+        <input
+          type="password"
+          placeholder="password"
+          id="password"
+          onChange={handleChange}
+          className="lInput"
+        />
+        <button disabled={loading} onClick={handleClick} className="lButton">
+          Login
+        </button>
+        {/* {error && <span>{error.message}</span>} */}
+      </div>
     </div>
-  </div>;
+  );
 };
 
 export default Login;
