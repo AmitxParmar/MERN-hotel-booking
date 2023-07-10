e0811cb0d4dcb6a820a48ab4377ec2fa60fab7fb
AmitxParmar <amitparmar901@gmail.com>
2023-07-10 22:49:33 +0530

    complete:fetchHook

--- client/src/hooks/useFetch.ts
+++ client/src/hooks/useFetch.ts
@@ -1,21 +1,47 @@
 import { useEffect, useState } from "react";
 import axios from 'axios';
 
-const useFetch = (url: string) => {
-    const [data, setData] = useState([]);
+interface FetchData<T> {
+    data: T[];
+    loading: boolean;
+    error: boolean | T;
+    reFetch: () => Promise<void>;
+}
+
+const useFetch = <T>(url: string): FetchData<T> => {
+    const [data, setData] = useState<T[]>([]);
     const [loading, setLoading] = useState<boolean>(false);
-    const [error, setError] = useState(false);
+    const [error, setError] = useState<boolean>(false);
 
     useEffect(() => {
         const fetchData = async () => {
             setLoading(true);
             try {
-                const res = await axios.get(url);
+                const res = await axios.get<T[]>(url)
                 setData(res.data);
             } catch (err) {
-                setError(err);
+                setError(true);
             }
+            setLoading(false);
+        };
+        void fetchData();
+    }, [url]);
+
+    // refetch
+    const reFetch = async () => {
+        setLoading(true);
+        try {
+            const res = await axios.get<T[]>(url);
+            setData(res.data)
+        } catch (err) {
+            setError(true)
         }
-    })
+        setLoading(false);
+    };
+
+    return { data, loading, error, reFetch };
+
 }
+
+export default useFetch;
 
