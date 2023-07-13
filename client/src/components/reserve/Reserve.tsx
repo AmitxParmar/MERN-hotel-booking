9e3690f366a43451d50473a7e0c973517013d691
AmitxParmar <amitparmar901@gmail.com>
2023-07-13 23:57:45 +0530

    complete:Hotel->Reserve

--- client/src/components/reserve/Reserve.tsx
+++ client/src/components/reserve/Reserve.tsx
@@ -1,5 +1,12 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./reserve.css";
-import React, { Dispatch, SetStateAction } from "react";
+import { useState, type Dispatch, type SetStateAction } from "react";
+import { faCircleXmark } from "@fortawesome/free-solid-svg-icons";
+import useFetch from "@/hooks/useFetch";
+import { IRoom } from "@/types";
+import { useSearch } from "@/common/search.store";
+import { useNavigate } from "react-router-dom";
+
 const Reserve = ({
   setOpen,
   hotelId,
@@ -7,7 +14,97 @@ const Reserve = ({
   setOpen: Dispatch<SetStateAction<boolean>>;
   hotelId: string;
 }) => {
-  return <div>Reserve</div>;
+  const dates = useSearch((store) => store.dates);
+  const [selectedRooms, setSelectedRooms] = useState<string[]>([]);
+
+  const {
+    data: rooms,
+    loading: isRoomLoading,
+    error,
+  } = useFetch<IRoom[]>(`/api/hotels/room/${hotelId}`);
+
+  const navigate = useNavigate();
+
+  const getDatesInRange = (startDate: Date, endDate: Date) => {
+    const start = new Date(startDate);
+    const end = new Date(endDate);
+
+    const date = new Date(start.getTime());
+
+    const dates = [];
+
+    while (date <= end) {
+      dates.push(new Date(date).getTime());
+      date.setDate(date.getDate() + 1);
+    }
+    return dates;
+  };
+
+  const alldates = getDatesInRange(dates[0].startDate, dates[0].endDate);
+
+  const isAvailable = (roomNumber: {
+    number: number;
+    unavailableDates: Date[];
+  }) => {
+    const isFound = roomNumber.unavailableDates.some((date) =>
+      alldates.includes(new Date(date).getTime())
+    );
+    return !isFound;
+  };
+
+  const handleSelect = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const checked = e.target.checked;
+    const value = e.target.value;
+    setSelectedRooms(
+      checked
+        ? [...selectedRooms, value]
+        : selectedRooms.filter((item: string) => item !== value)
+    );
+  };
+  const handleClick = async () => {
+    try {
+      await Promise.all(
+        selectedRooms.map((roomId) => {
+          const res = axios.put(`/api/rooms/availability/${roomId}`, {
+            dates: alldates,
+          });
+          return res.data;
+        })
+      );
+      setOpen(false);
+      navigate("/");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <div className="reserve">
+      <div className="rContainer">
+        <FontAwesomeIcon
+          icon={faCircleXmark}
+          className="rClose"
+          onClick={() => setOpen(false)}
+        />
+        <span>Select your rooms:</span>
+        {rooms?.map((room) => (
+          <div className="rItem" key={room._id}>
+            <div className="rItemInfo">
+              <div className="rTitle">{room.title}</div>
+              <div className="rDesc">{room.desc}</div>
+              <div className="rMax">
+                Max people: <b>{room.maxPeople}</b>
+              </div>
+              <div className="rPrice">{room.price}</div>
+            </div>
+            <div className="rselectRooms">
+              {rooms.roomNumbers.}
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Reserve;
