cba74b8798162c644e599144c3d42de521710e4c
AmitxParmar <amitparmar901@gmail.com>
2023-07-16 22:58:04 +0530

    complete:Admin->Featured

--- client/src/pages/admin/home/Home.tsx
+++ client/src/pages/admin/home/Home.tsx
@@ -1,28 +1,28 @@
+import { useNavigate } from "react-router-dom";
+
 import Navbar from "@/components/admin/navbar/Navbar";
 import Sidebar from "@/components/admin/sidebar/Sidebar";
-import { useNavigate } from "react-router-dom";
-import axios from "axios";
 import Widget from "@/components/admin/widget/Widget";
+import Featured from "@/components/admin/featured/Featured";
 
 const AdminHome = () => {
-  /*  const getData = async (): Promise<void> => {
-    await axios
-      .get("https://pokeapi.co/api/v2/pokemon/ditto")
-      .then((data) => console.log(data, "pokemon"));
-  };
-
-  */ const navigate = useNavigate();
+  const navigate = useNavigate();
   return (
     <div className="home">
-      {/* <button onClick={() => void getData()}>getData</button> */}
       <Sidebar />
       <div className="homeContainer">
         <Navbar />
         <div className="widgets">
-          <Widget type="user"/>
-          <Widget type="order"/>
-          <Widget type="earning"/>
-          <Widget type="balance"/>
+          <Widget type="user" />
+          <Widget type="order" />
+          <Widget type="earning" />
+          <Widget type="balance" />
+        </div>
+        <div className="charts">
+          <Featured />
+        </div>
+        <div className="listContainer">
+          <div className="listTitle">Latest Transactions</div>
         </div>
       </div>
     </div>
